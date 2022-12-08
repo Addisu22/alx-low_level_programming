393e3a7d0cf7235feb422ee56d86b05906954ca9
Addisu22 <liveaddisu@gmail.com>
2022-12-08 07:19:34 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - The output has  a string to stdout.
+ * Return: 0 if the program file is successful.
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
