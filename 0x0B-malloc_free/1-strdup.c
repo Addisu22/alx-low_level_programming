8db7cca4ff23d9634f93bea10be630da1bd251ce
Addisu22 <117830752+Addisu22@users.noreply.github.com>
2023-01-09 15:03:49 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,30 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 /**
- * _strdup - duplicate to new memory space location
- * @str: char
- * Return: 0
+ * _strlen - give the length of a string
+ * @s: the string
+ *
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		;
+	return (i);
+}
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strdup - returns a pointer which contains a copy of the string
+ * given as a parameter
+ * @str: The string to print
+ *
+ *
+ * Return: a pointer to the duplicated string or NULL if insufficient
+ * memory was available
  */
 char *_strdup(char *str)
 {
-	int i, end;
-	char *array;
+	int i;
+	char *copy;
 
-	if (str == NULL)
-		return (NULL);
-
-	for (end = 0; end <= *str; end++)
+	if (str == '\0')
 	{
+		return ('\0');
 	}
-
-	end += 1;
-	array = malloc(sizeof(char) * end);
-
-	for (i = 0; i < end; i++)
-		array[i] = str[i];
-
-	if (array == NULL)
-		return (NULL);
-	return (array);
-}
+	i = _strlen(str);
+	copy = malloc(sizeof(char) * i + 1);
+	if (copy == '\0')
+	{
+		return ('\0');
+	}
+	else
+	{
+		for (i = 0 ; str[i] != '\0' ; i++)
+		{
+			copy[i] = str[i];
+		}
+	}
+	copy[i] = '\0';
+	return (copy);
